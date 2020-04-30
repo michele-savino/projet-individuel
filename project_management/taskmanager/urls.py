5174a13f9a260997af838639aa5538affd1da437
michele-savino <michele-pio.savino@studenti.isae-supaero.fr>
2020-04-30 09:29:06 +0200

    Page de visualisation des taches créée

--- project_management/taskmanager/urls.py
+++ project_management/taskmanager/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('projects', views.projects, name='user_projects')
+    path('projects', views.projects_list, name='projects_list'),
+    path('project/<int:id>', views.show_project, name='project'),
 ]
--- project_management/taskmanager/views.py
+++ project_management/taskmanager/views.py
@@ -1,13 +1,21 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Project
 
 
 @login_required(redirect_field_name='redirected_to')
-def projects(request):
+def projects_list(request):
     current_user = request.user
     user_projects = current_user.project_set.all()
 
-    return render(request, 'taskmanager/projects.html', locals())
+    return render(request, 'taskmanager/projects_list.html', locals())
+
+
+@login_required(redirect_field_name='redirected_to')
+def show_project(request, id):
+    project = get_object_or_404(Project, id=id)
+    tasks = project.task_set.all()
+
+    return render(request, 'taskmanager/project.html', locals())
