5c8e4a21dc2d9a8a558117fe9fd5523b1b7a4000
michele-savino <michele-pio.savino@studenti.isae-supaero.fr>
2020-05-04 16:40:53 +0200

    Model and datetime mieux definis

--- project_management/taskmanager/models.py
+++ project_management/taskmanager/models.py
@@ -1,15 +1,12 @@
+from datetime import date
+
 from django.contrib.auth.models import User
-from django.core.validators import MaxValueValidator, MinValueValidator
+from django.core.exceptions import ValidationError
 from django.db import models
-from django.utils import timezone
 
-
-## aggiustare gli attributi dei caratteri in funzione del risultato finale
-# negli attributi data timezone.now non va bene perchè non tiene in conto dei fusi
-# AGGIUNGERE SLUUUUUG
 
 class Project(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=100)
     members = models.ManyToManyField(User)
 
     def __str__(self):
@@ -17,38 +14,46 @@ class Project(models.Model):
 
 
 class Status(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=30, unique=True) # je veux de status unique
 
     class Meta:
-        verbose_name_plural = 'status'
+        verbose_name_plural = 'status'  # pour ne pas avoir 2 s au plural
 
     def __str__(self):
         return self.name
 
 
+# valeurs de default
 DEFAULT_STATUS_ID = 1
 DEFAULT_PRIORITY = 1
 
 
-class Task(models.Model):
-    # fare in modo che solo i membri del progetto possano essere assegnati al task e non tutti
+# pour s'assurer que la priorité de la tache soit comprise entre 1 et 10
+def validate_priority(value):
+    if value < 1 or value > 10:
+        raise ValidationError('Ensure this value is between 1 and 10')
 
+
+class Task(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
     assignee = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Assigned to")
-    start_date = models.DateField(default=timezone.now)
-    due_date = models.DateField(default=timezone.now)
-    priority = models.SmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)], default=DEFAULT_PRIORITY)
-    status = models.ForeignKey(Status, on_delete=models.CASCADE, default=DEFAULT_STATUS_ID)
+    start_date = models.DateField(default=date.today)
+    due_date = models.DateField(default=date.today)
+    priority = models.SmallIntegerField(validators=[validate_priority],
+                                        default=DEFAULT_PRIORITY)
+    # si le status est supprimé, la tache n'est pas supprimé
+    status = models.ForeignKey(Status, on_delete=models.SET_DEFAULT, default=DEFAULT_STATUS_ID)
 
     def __str__(self):
         return self.name
 
 
 class Journal(models.Model):
-    date = models.DateField(default=timezone.now)
-    entry = models.CharField(max_length=200)
+    # date et heure fixées au moment de la création
+    date = models.DateTimeField(auto_now_add=True)
+    entry = models.CharField(max_length=240)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     task = models.ForeignKey(Task, on_delete=models.CASCADE)
 
