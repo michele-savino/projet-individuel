86ee5934d322c3e8eeec9aa040bd5322a2fd81f2
michele-savino <michele-pio.savino@studenti.isae-supaero.fr>
2020-05-04 09:18:09 +0200

    Changé date widget dans le TaskForm

--- project_management/taskmanager/forms.py
+++ project_management/taskmanager/forms.py
@@ -9,7 +9,12 @@ class JournalForm(forms.ModelForm):
         fields = ['entry']
 
 
+#controllare se la data di fine è dopo quella di inizio
+
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ["name", "assignee", "description", "start_date", "due_date", "priority", "status"]
+        widgets = {'description': forms.Textarea(attrs={'rows': 4}),
+                   'start_date': forms.SelectDateWidget,
+                   'due_date': forms.SelectDateWidget,}
